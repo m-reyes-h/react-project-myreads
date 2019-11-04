a56240328611a308094cd4af0457994862dabe32
Michel Reyes <mreyesh85+1@gmail.com>
2019-11-03 22:50:01 -0500

    Fixed search with debounce

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -3,32 +3,48 @@ import BookSearchNavbar from "./BookSearchNavbar";
 import * as DB from "../data/BooksAPI";
 import Book from "./Book";
 import propTypes from "prop-types";
+import { throttle, debounce } from "throttle-debounce";
 
 class BookSearch extends Component {
   state = {
     searchTerm: "",
     books: [],
     loading: false,
-    error: false
+    error: false,
+    searchTermFinded: ""
   };
 
+  searchDebounced = debounce(500, query => this.searchBooks(query));
+
   changeTerms = term => {
-    this.setState({ searchTerm: term });
-    this.searchBooks(term);
+    this.setState({ searchTerm: term }, () => {
+      const query = this.state.searchTerm;
+      this.searchDebounced(query);
+    });
   };
 
   searchBooks = term => {
     const query = term.trim();
 
     if (!query.length) {
-      return this.setState(() => ({ books: [], loading: false, error: false }));
+      return this.setState(() => ({
+        books: [],
+        loading: false,
+        error: false,
+        searchTermFinded: query
+      }));
     }
 
     this.setState(() => ({ loading: true }));
 
     DB.search(query, 20)
       .then(books => {
-        this.setState(() => ({ books, loading: false, error: false }));
+        this.setState(prevState => ({
+          books,
+          loading: false,
+          error: false,
+          searchTermFinded: prevState.searchTerm
+        }));
       })
       .catch(err => {
         this.setState(() => ({ error: true }));
@@ -42,12 +58,12 @@ class BookSearch extends Component {
   };
 
   render() {
-    const { books, searchTerm } = this.state;
+    const { books, searchTerm, searchTermFinded } = this.state;
 
     return (
       <div className="search-books">
         <BookSearchNavbar changeTerms={this.changeTerms} />
-        {this.state.loading ? (
+        {this.state.loading || searchTerm !== searchTermFinded ? (
           <p className="search-books-info">Loding...</p>
         ) : (
           <div>
@@ -69,12 +85,14 @@ class BookSearch extends Component {
                 </div>
               </div>
             )}
-            {books.error && searchTerm.trim().length > 0 && (
-              <h3 className="search-books-info">
-                Search <span className="tag">{searchTerm}</span> did not return
-                any books. Please try again!
-              </h3>
-            )}
+            {books.error &&
+              searchTerm.trim().length > 0 &&
+              searchTerm === searchTermFinded && (
+                <h3 className="search-books-info">
+                  Search <span className="tag">{searchTermFinded}</span> did not
+                  return any books. Please try again!
+                </h3>
+              )}
           </div>
         )}
       </div>
