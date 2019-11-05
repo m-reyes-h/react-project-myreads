6ad7dd8e2d75e8ef3e2127519bddafa36c367fd2
Michel Reyes <mreyesh85+1@gmail.com>
2019-11-04 21:46:45 -0500

    Updates

---
+++ precache-manifest.aff18a8d33ff67d4968af176dde57227.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec3151ac4a09ae494e27c457f4602a74",
+    "url": "/m-reyes-h/react-project-myreads/static/media/arrow-drop-down.ec3151ac.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/m-reyes-h/react-project-myreads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "a768b23d02fe7e39b2144b353eb2d801",
+    "url": "/m-reyes-h/react-project-myreads/static/media/add.a768b23d.svg"
+  },
+  {
+    "revision": "51db69042cdfe6c6bd6e",
+    "url": "/m-reyes-h/react-project-myreads/static/js/runtime~main.51db6904.js"
+  },
+  {
+    "revision": "ad8b2d4a21e50e9d5ac4",
+    "url": "/m-reyes-h/react-project-myreads/static/js/main.ad8b2d4a.chunk.js"
+  },
+  {
+    "revision": "beb79b1b11c15d111254",
+    "url": "/m-reyes-h/react-project-myreads/static/js/1.beb79b1b.chunk.js"
+  },
+  {
+    "revision": "ad8b2d4a21e50e9d5ac4",
+    "url": "/m-reyes-h/react-project-myreads/static/css/main.8c79d1d7.chunk.css"
+  },
+  {
+    "revision": "49213977958b7f12bcd79de4c1f1731a",
+    "url": "/m-reyes-h/react-project-myreads/index.html"
+  }
+];
